000da469fa6e441fbccf7fd6af029ec6bfc30391
Michal Grudzien <michal.grudzien1@gmail.com>
2019-05-27 22:45:14 +0200

    Set up website config, delete unnecessary files, clean up starter

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,10 @@
+const path = require("path");
+
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Dobra Praktyka Żeglarska`,
+    description: `Ilość przepłynięcia jakiej jeszcze nie widziałeś.`,
+    author: `@depezet`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -10,7 +12,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
+        path: `${__dirname}/src/assets/images`,
       },
     },
     `gatsby-transformer-sharp`,
@@ -19,16 +21,13 @@ module.exports = {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `gatsby-starter-default`,
-        short_name: `starter`,
+        short_name: `DPŻ`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/assets/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
-}
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,12 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from "react";
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import SEO from "components/SEO";
 
 const IndexPage = () => (
-  <Layout>
+  <>
     <SEO title="Home" />
-  </Layout>
-)
+    ELO
+  </>
+);
 
-export default IndexPage
+export default IndexPage;
