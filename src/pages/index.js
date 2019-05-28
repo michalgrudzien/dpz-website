4e98bf6c2329daf82c80ea9e591e59fab4e67272
Michal Grudzien <michal.grudzien1@gmail.com>
2019-05-28 23:01:13 +0200

    Add fonts and move them to static folder

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { Reset } from "styled-reset";
+import GlobalStyle from "settings/GlobalStyle";
 import SEO from "components/SEO";
 
 const IndexPage = () => (
   <>
     <Reset />
-    <SEO title="Home" />
-    ELO
+    <GlobalStyle />
+    <SEO title="Idzie nowe" />
+    <p>ELO</p>
+    <h1>IDZIE NOWE</h1>
   </>
 );
 
---
+++ src/settings/fonts.js
@@ -0,0 +1,44 @@
+export default `
+@import url("https://fonts.googleapis.com/css?family=Raleway:100,300,400,400i,500,500i,600,700,800,900&display=swap&subset=latin-ext")
+
+    @font-face {
+    font-family: 'Bebas Neue Book';
+    font-style: normal;
+    font-weight: normal;
+    src: url(fonts/BebasNeueBook.woff) format('woff');
+    }
+
+    @font-face {
+    font-family: 'Bebas Neue Regular';
+    font-style: normal;
+    font-weight: normal;
+    src: url(fonts/BebasNeueRegular.woff) format('woff');
+    }
+    
+    @font-face {
+    font-family: 'Bebas Neue Regular';
+    font-style: normal;
+    font-weight: normal;
+    src: url('fonts/BebasNeue.woff') format('woff');
+    }
+    
+    @font-face {
+    font-family: 'Bebas Neue Thin';
+    font-style: normal;
+    font-weight: normal;
+    src: url('fonts/BebasNeueThin.woff') format('woff');
+    }
+    
+    @font-face {
+    font-family: 'Bebas Neue Light';
+    font-style: normal;
+    font-weight: normal;
+    src: url('fonts/BebasNeueLight.woff') format('woff');
+    }
+    
+    @font-face {
+    font-family: 'Bebas Neue Bold';
+    font-style: normal;
+    font-weight: normal;
+    src: url('fonts/BebasNeueBold.woff') format('woff');
+    }  `;
