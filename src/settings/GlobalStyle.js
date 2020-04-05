b18439059985dcd325aeabcb6bfd9ab5f1f4f7d5
Michał Grudzień <michal.grudzien1@gmail.com>
2020-04-05 19:48:37 +0200

    Remove debug styling

--- src/settings/GlobalStyle.js
+++ src/settings/GlobalStyle.js
@@ -12,8 +12,6 @@ const GlobalStyle = createGlobalStyle`
     font-family: 'Raleway', sans-serif !important;  
     color: ${colors.default};
     font-size: 16px;
-    height: 4000px;
-    background-color: #ddd;
   }
 
   h1,h2,h3,h4,h5,h6{
