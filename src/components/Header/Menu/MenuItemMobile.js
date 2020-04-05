b0131fe123f7d54a52636623fabba6ecdf1bbfdf
Michał Grudzień <michal.grudzien1@gmail.com>
2020-04-05 19:47:56 +0200

    Fix lint warnings

--- src/components/Header/Menu/MenuItemMobile.js
+++ src/components/Header/Menu/MenuItemMobile.js
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "gatsby";
 import PropTypes from "prop-types";
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 
 import colors from "settings/colors";
 
