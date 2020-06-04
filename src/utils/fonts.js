ee27e7440d2e4a0c16e8fb20c2303b427b5c7848
Michał Grudzień <michal.grudzien1@gmail.com>
2020-06-04 14:25:51 +0200

    Fix fonts paths to absolute

--- src/utils/fonts.js
+++ src/utils/fonts.js
@@ -5,41 +5,41 @@ export default `
     font-family: 'Bebas Neue Book';
     font-style: normal;
     font-weight: normal;
-    src: url(fonts/BebasNeueBook.woff) format('woff');
+    src: url(/fonts/BebasNeueBook.woff) format('woff');
     };
 
     @font-face {
     font-family: 'Bebas Neue Regular';
     font-style: normal;
     font-weight: normal;
-    src: url(fonts/BebasNeueRegular.woff) format('woff');
+    src: url(/fonts/BebasNeueRegular.woff) format('woff');
     };
     
     @font-face {
     font-family: 'Bebas Neue Regular';
     font-style: normal;
     font-weight: normal;
-    src: url('fonts/BebasNeue.woff') format('woff');
+    src: url('/fonts/BebasNeue.woff') format('woff');
     };
     
     @font-face {
     font-family: 'Bebas Neue Thin';
     font-style: normal;
     font-weight: normal;
-    src: url('fonts/BebasNeueThin.woff') format('woff');
+    src: url('/fonts/BebasNeueThin.woff') format('woff');
     };
     
     @font-face {
     font-family: 'Bebas Neue Light';
     font-style: normal;
     font-weight: normal;
-    src: url('fonts/BebasNeueLight.woff') format('woff');
+    src: url('/fonts/BebasNeueLight.woff') format('woff');
     };
     
     @font-face {
     font-family: 'Bebas Neue Bold';
     font-style: normal;
     font-weight: normal;
-    src: url('fonts/BebasNeueBold.woff') format('woff');
+    src: url('/fonts/BebasNeueBold.woff') format('woff');
     };  
   `;
